1970ae5deb9dc8c9df33ebeacccaf5e2a8749d98
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-11-14 20:25:00 +0000

    feat: add ActReact service

--- crates/osm/src/services.rs
+++ crates/osm/src/services.rs
@@ -1,16 +1,18 @@
 use std::{
-    ffi::{CString, c_int},
+    ffi::{CStr, CString, c_float, c_int},
     os::raw::c_char,
+    ptr::null,
     str::FromStr,
 };
 
 use windows::core::*;
 
-use crate::IScriptMan;
+use crate::{IScriptMan, sMultiParm};
 
 static mut SERVICES: Option<&Services> = None;
 
 pub struct Services {
+    pub act_react: ActReactService,
     pub debug: DebugService,
     pub version: VersionService,
 }
@@ -21,6 +23,9 @@ pub fn services() -> &'static Services {
 
 pub(crate) fn services_init(script_manager: IScriptMan) {
     let services = Services {
+        act_react: ActReactService {
+            service: get_service(&script_manager),
+        },
         debug: DebugService {
             service: get_service(&script_manager),
         },
@@ -33,6 +38,170 @@ pub(crate) fn services_init(script_manager: IScriptMan) {
 
 fn get_service<T: Interface>(script_manager: &IScriptMan) -> T {
     unsafe { script_manager.GetService(&T::IID).cast::<T>().unwrap() }
+}
+
+#[interface("F40000F4-7B74-12C3-8348-00AA00A82B51")]
+unsafe trait IActReactServiceT1: IUnknown {
+    fn Init(&self);
+    fn End(&self);
+    fn React(
+        &self,
+        what: c_int,
+        stim_intensity: c_int,
+        target: c_int,
+        agent: c_int,
+        parm1: *const sMultiParm,
+        parm2: *const sMultiParm,
+        parm3: *const sMultiParm,
+        parm4: *const sMultiParm,
+        parm5: *const sMultiParm,
+        parm6: *const sMultiParm,
+        parm7: *const sMultiParm,
+        parm8: *const sMultiParm,
+    ) -> HRESULT;
+    fn StimulateLegacy(&self, who: c_int, what: c_int, how_much: c_float) -> HRESULT;
+    fn GetReactionNamed(&self, name: *const c_char) -> c_int;
+    fn GetReactionName(&self, id: c_int) -> *const c_char;
+    fn SubscribeToStimulus(&self, obj: c_int, what: c_int) -> HRESULT;
+    fn UnsubscribeToStimulus(&self, obj: c_int, what: c_int) -> HRESULT;
+    fn BeginContact(&self, source: c_int, sensor: c_int) -> HRESULT;
+    fn EndContact(&self, source: c_int, sensor: c_int) -> HRESULT;
+    fn SetSingleSensorContact(&self, source: c_int, sensor: c_int) -> HRESULT;
+    fn Stimulate(&self, who: c_int, what: c_int, how_much: c_float, source: c_int) -> HRESULT;
+}
+
+#[interface("F40000F4-7B74-12C3-8348-00AA00A82B51")]
+unsafe trait IActReactService: IUnknown {
+    fn Init(&self);
+    fn End(&self);
+    fn React(
+        &self,
+        what: c_int,
+        stim_intensity: c_int,
+        target: c_int,
+        agent: c_int,
+        parm1: *const sMultiParm,
+        parm2: *const sMultiParm,
+        parm3: *const sMultiParm,
+        parm4: *const sMultiParm,
+        parm5: *const sMultiParm,
+        parm6: *const sMultiParm,
+        parm7: *const sMultiParm,
+        parm8: *const sMultiParm,
+    ) -> HRESULT;
+    fn Stimulate(&self, who: c_int, what: c_int, how_much: c_float, source: c_int) -> HRESULT;
+    fn GetReactionNamed(&self, name: *const c_char) -> c_int;
+    fn GetReactionName(&self, id: c_int) -> *const c_char;
+    fn SubscribeToStimulus(&self, obj: c_int, what: c_int) -> HRESULT;
+    fn UnsubscribeToStimulus(&self, obj: c_int, what: c_int) -> HRESULT;
+    fn BeginContact(&self, source: c_int, sensor: c_int) -> HRESULT;
+    fn EndContact(&self, source: c_int, sensor: c_int) -> HRESULT;
+    fn SetSingleSensorContact(&self, source: c_int, sensor: c_int) -> HRESULT;
+}
+
+pub struct ActReactService {
+    #[cfg(game = "t1")]
+    service: IActReactServiceT1,
+    #[cfg(not(game = "t1"))]
+    service: IActReactService,
+}
+
+impl ActReactService {
+    pub fn react(
+        &self,
+        what: i32,
+        stim_intensity: i32,
+        target: Option<i32>,
+        agent: Option<i32>,
+        parm1: Option<sMultiParm>,
+        parm2: Option<sMultiParm>,
+        parm3: Option<sMultiParm>,
+        parm4: Option<sMultiParm>,
+        parm5: Option<sMultiParm>,
+        parm6: Option<sMultiParm>,
+        parm7: Option<sMultiParm>,
+        parm8: Option<sMultiParm>,
+    ) {
+        let _ = unsafe {
+            self.service.React(
+                what,
+                stim_intensity,
+                target.unwrap_or(0),
+                agent.unwrap_or(0),
+                match parm1 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm2 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm3 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm4 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm5 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm6 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm7 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+                match parm8 {
+                    Some(p) => &p,
+                    None => null(),
+                },
+            )
+        };
+    }
+
+    pub fn stimulate(&self, who: i32, what: i32, how_much: f32, source: Option<i32>) {
+        let _ = unsafe {
+            self.service
+                .Stimulate(who, what, how_much, source.unwrap_or(0))
+        };
+    }
+
+    pub fn get_reaction_named(&self, name: &str) -> i32 {
+        let name = CString::new(name).unwrap();
+        unsafe { self.service.GetReactionNamed(name.as_ptr()) }
+    }
+
+    pub fn get_reaction_name(&self, id: i32) -> String {
+        unsafe {
+            let name = self.service.GetReactionName(id);
+            CStr::from_ptr(name).to_str().unwrap().to_string()
+        }
+    }
+
+    pub fn subscribe_to_stimulus(&self, obj: i32, what: i32) {
+        let _ = unsafe { self.service.SubscribeToStimulus(obj, what) };
+    }
+
+    pub fn unsubscribe_to_stimulus(&self, obj: i32, what: i32) {
+        let _ = unsafe { self.service.UnsubscribeToStimulus(obj, what) };
+    }
+
+    pub fn begin_contact(&self, source: i32, sensor: i32) {
+        let _ = unsafe { self.service.BeginContact(source, sensor) };
+    }
+
+    pub fn end_contact(&self, source: i32, sensor: i32) {
+        let _ = unsafe { self.service.EndContact(source, sensor) };
+    }
+
+    pub fn set_single_sensor_contact(&self, source: i32, sensor: i32) {
+        let _ = unsafe { self.service.SetSingleSensorContact(source, sensor) };
+    }
 }
 
 #[interface("D70000D7-7B57-12A6-8348-00AA00A82B51")]
